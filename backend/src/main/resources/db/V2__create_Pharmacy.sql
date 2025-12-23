f802be96e8976c5533fe7d2ed1dbf63bdac71e6b
Nabanit Mondal <navy190899@gmail.com>
2025-12-23 11:34:41 +0530

    Update SQL script for Pharmacy

--- backend/src/main/resources/db/V2__create_Pharmacy.sql
+++ backend/src/main/resources/db/V2__create_Pharmacy.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS pharmacy (
-    id BIGINT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     address VARCHAR(255),
     phone VARCHAR(255),
     fax VARCHAR(255),
     logo_url VARCHAR(255),
     subscription_status VARCHAR(255),
-    created_at TIMESTAMP(6) WITHOUT TIME ZONE
+    subscription_duration_started_at TIMESTAMP,
+    subscription_duration_expire_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL
 );
+
 
