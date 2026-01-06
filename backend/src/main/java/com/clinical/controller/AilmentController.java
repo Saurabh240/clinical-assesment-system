c2b4dc93ba7c328e11ee0e0968aaec7981ce8f58
Nabanit Mondal <navy190899@gmail.com>
2026-01-06 16:43:05 +0530

    Designed and tested the APIs for Ailments, also included the API docs and SQL script

---
+++ backend/src/main/java/com/clinical/controller/AilmentController.java
@@ -0,0 +1,30 @@
+package com.clinical.controller;
+
+import com.clinical.dto.AilmentRequest;
+import com.clinical.service.AilmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ailments")
+@RequiredArgsConstructor
+public class AilmentController {
+
+    private final AilmentService ailmentService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllAilments(){
+        return ResponseEntity.ok(ailmentService.getAllAilments());
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('PHARMACY_ADMIN')")
+    public ResponseEntity<?> createOrUpdateAilment(
+            @Valid
+            @RequestBody AilmentRequest ailmentRequest){
+        return ResponseEntity.ok(ailmentService.createOrUpdateAilment(ailmentRequest));
+    }
+}
