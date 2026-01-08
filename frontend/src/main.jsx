9d14ac6cb769c821090f3a783e4482c92aafe84f
Awadhesh Kumar <awadhesh23th@gmail.com>
2026-01-08 19:55:04 +0530

    Changes for assessment route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
-import { AssessmentProvider } from "./context/AssessmentContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <AssessmentProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-      </AssessmentProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
