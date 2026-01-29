b4b39afd0129107390908ed0b872f73a4a7360a5
Nandini016 <nandinis2216@gmail.com>
2026-01-29 17:59:03 +0530

    changes in paginationProperties

--- backend/src/main/java/com/clinical/config/PaginationProperties.java
+++ backend/src/main/java/com/clinical/config/PaginationProperties.java
@@ -1,12 +1,12 @@
 package com.clinical.config;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
+@Data
 @Component
 @ConfigurationProperties(prefix = "app.pagination")
 public class PaginationProperties {
