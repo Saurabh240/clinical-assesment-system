e3c8585bc5634baf33dc4bd2b091ae0971030088
nandini016 <nandinis2216@gmail.com>
2026-03-10 16:46:36 +0530

    19 minor ailments template implementation

--- backend/src/main/java/com/clinical/service/PdfHtmlService.java
+++ backend/src/main/java/com/clinical/service/PdfHtmlService.java
@@ -46,9 +46,42 @@ public class PdfHtmlService {
 
             case "UTI" ->
                     "assessments/fragments/ailments/uti :: section";
-
             case "OSELTAMIVIR" ->
                     "assessments/fragments/ailments/tamiflu :: section";
+            case "DERMATITIS" ->
+                    "assessments/fragments/ailments/dermatitis :: section";
+            case "DYSMENORRHEA" ->
+                    "assessments/fragments/ailments/dysmenorrhea :: section";
+            case "INSECT_BITES" ->
+                    "assessments/fragments/ailments/insect-bites :: section";
+            case "CONJUNCTIVITIS" ->
+                    "assessments/fragments/ailments/conjunctivitis :: section";
+            case "HEMORRHOIDS" ->
+                    "assessments/fragments/ailments/impetigo :: section";
+            case "TICK_BITES" ->
+                    "assessments/fragments/ailments/tickbites :: section";
+            case "ALLERGIC_RHINITIS" ->
+                    "assessments/fragments/ailments/allergic_rhinitis :: section";
+            case "COLD_SORE" ->
+                    "assessments/fragments/ailments/cold_sore :: section";
+            case "GERD" ->
+                    "assessments/fragments/ailments/gerd :: section";
+            case "MUSCULOSKELETAL_SPRAINS" ->
+                    "assessments/fragments/ailments/musculoskeletal_sprains :: section";
+            case "CANDIDAL_STOMATITIS" ->
+                    "assessments/fragments/ailments/candidal_stomatits :: section";
+            case "ACNE" ->
+                    "assessments/fragments/ailments/acne :: section";
+            case "APHTHOUS_ULCERS" ->
+                    "assessments/fragments/ailments/aphthous_ulcers :: section";
+            case "NAUSEA_VOMITING_PREGNANCY" ->
+                    "assessments/fragments/ailments/nausea_vomiting_pregnancy :: section";
+            case "PINWORMS" ->
+                    "assessments/fragments/ailments/pinworms :: section";
+            case "VAGINAL_CANDIDIASIS" ->
+                    "assessments/fragments/ailments/vaginal_candidiasis :: section";
+            case "DIAPER_DERMATITIS" ->
+                    "assessments/fragments/ailments/diaper_dermatitis :: section";
 
             default ->
                     "assessments/fragments/ailments/default :: section";
